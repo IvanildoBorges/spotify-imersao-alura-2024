32de5270db9d6b7fa16f548b4c607030f2914353
IvanildoBorges <ivanildo.rap@gmail.com>
2024-01-27 19:56:38 -0300

    Adicionado script e como rodar o projeto

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spotify-imersao-alura-2024
 Construindo um Streaming de música
+
+## RUN
+1. Instale o NodeJS no seu pc
+
+2. Abra o prompt de comando (CMD) e baixe o pacote JSON SERVER com o código abaixo:
+npm i json-server -g
+
+3. Abra o prompt de comando e utilize o código abaixo para simular uma API:
+json-serve --watch caminho_do_arquivo --port 3000
+
+4. Agora é só abrir o projeto com o plugin chamado Live Server do editor de texto chamado Visual Studio Code (VS Code) e testar seu projeto ^^
---
+++ script.js
@@ -0,0 +1,75 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+async function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists`
+    let resultados = []
+    let artistasEncontrados = []
+    
+    let listaDeNomes = await fetch(url)
+        .then((response) => response.json())
+        .then((result) => result.map((item) => item.name))
+
+    listaDeNomes = listaDeNomes.filter((nome) => {
+        if (nome.toLowerCase().includes(searchTerm)) {
+            return nome;
+        }
+    })
+
+    for (let i = 0; i < listaDeNomes.length; i++) {
+        resultados.push(
+            await fetch(`${url}?name=${listaDeNomes[i]}`)
+                .then((response) => response.json())
+                .then((result) => result)
+        )
+    }
+
+    resultados.forEach(element => {
+        artistasEncontrados.push(Object.assign({}, element))
+    })
+
+    displayResults(artistasEncontrados)
+}
+
+function displayResults(result) {
+    resultArtist.classList.remove('hidden');
+    let containerDeCards = document.querySelector('.grid-container')
+
+    const card = result.map(element => `
+            <div class="artist-card" id="">
+                <div class="card-img">
+                    <img src="${element[0].urlImg}" id="artist-img" class="artist-img" />
+                    <div class="play">
+                        <span class="fa fa-solid fa-play"></span>
+                    </div>
+                </div>
+                <div class="card-text">
+                    <a title="Foo Fighters" class="vst" href="">
+                        <span class="artist-name" id="artist-name">${element[0].name}</span>
+                        <span class="artist-categorie">Artista</span>
+                    </a>
+                </div>
+            </div>
+        `
+    )
+
+    containerDeCards.innerHTML = card
+
+    resultPlaylist.classList.add("hidden")
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+
+    if (searchTerm != '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+
+        requestApi(searchTerm);
+    } else {
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+    }
+    
+})
